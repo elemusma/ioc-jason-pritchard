70c84dabb37d9309520fcbaf696574b53690d91e
Efrain Lemus <efraintlemusm@gmail.com>
2022-10-31 11:40:13 -0600

    adjust single and index template

--- themes/insideoutcreative/partials/sidebar.php
+++ themes/insideoutcreative/partials/sidebar.php
@@ -1,40 +1,30 @@
-<div class="col-md-3 sidebar order-2 order-md-1">
-<div class="card p-3 mb-2">
-<?php get_search_form(); ?>
-</div>
-<div class="card p-3 mt-2 mb-2">
 <?php 
-// wp_list_categories(); 
+echo '<div class="col-md-3 sidebar order-2 order-md-1">';
+echo '<div class="card p-3 mb-2">';
+get_search_form();
+echo '</div>';
 
-echo '<h3>Categories</h3>';
-echo '<ul>';
-wp_list_categories( array('taxonomy' => 'product_cat', 'title_li'  => '') );
-echo '</ul>';
+// echo '<div class="card p-3 mt-2 mb-2">';
+// echo '<h3>Categories</h3>';
+// echo '<ul>';
+// wp_list_categories( array('taxonomy' => 'product_cat', 'title_li'  => '') );
+// echo '</ul>';
+// echo '</div>';
 
-?>
-<!-- <h3 class="teko">Authors</h3> -->
-<!-- <ul>
-<?php wp_list_authors('exclude_admin=0&optioncount=1&show_fullname=1&hide_empty=1'); ?>
-</ul> -->
-</div>
-<div class="card p-3 mt-2 mb-2">
-<h3 class="teko">Recent Posts</h3>
-<?php
+echo '<div class="card p-3 mt-2 mb-2">';
+echo '<h3 class="teko">Training Library</h3>';
+
 $recentBlog = new WP_Query(array(
-'posts_per_page' => 5,
-'post_type' => 'post'
-)); ?>
-<ul>
-<?php while($recentBlog->have_posts()){
+'posts_per_page' => 10,
+'post_type' => 'videos',
+'post__not_in' => [get_the_ID()]
+)); 
+echo '<ul>';
+while($recentBlog->have_posts()){
 $recentBlog->the_post(); ?>
 <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 <?php } wp_reset_postdata(); ?>
 </ul>
 </div>
-<!-- <div class="card p-3 mt-2 mb-2">
-<h3 class="teko">Archives</h3>
-<ul>
-<?php wp_get_archives(); ?>
-</ul>
-</div> -->
+
 </div>
--- themes/insideoutcreative/single.php
+++ themes/insideoutcreative/single.php
@@ -2,18 +2,19 @@
 <section class="pt-5 pb-5 body">
 <div class="container">
     <div class="row justify-content-center">
-    <?php get_template_part('partials/sidebar'); ?>
-        <div class="col-lg-9 col-md-12 order-lg-2 order-1">
-        <p class="d-flex flex-wrap align-items-center mb-5">
-        <?php echo get_avatar( get_the_author_meta('email'), '60', array( 'class' => array( 'mr-2', 'img-avatar' ) ) ); ?>
-    <span class="mr-1"><?php echo get_the_author_posts_link(); ?></span> | <?php the_time('F jS, Y'); ?></p>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+    <?php get_template_part('partials/sidebar');
+        echo '<div class="col-lg-9 col-md-12 order-lg-2 order-1">';
+        echo '<h1 class="mb-4">';
+        echo single_post_title();
+        echo '</h1>';
+        echo '<p class="mb-0">Posted on: ' . get_the_time('F jS, Y') . '</p>';
+        echo '<p class="">By: ' . get_the_author() . '</p>';
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
 the_content();
 endwhile; else: ?>
 <p>Sorry, no posts matched your criteria.</p>
 <?php endif; ?>
 <hr>
-<?php comments_template(); ?>
         </div>
         <!-- <div class="col-lg-3"> -->
             
